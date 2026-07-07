b6267e9b330bf346e59703156b417b08f31bce1a
bogdan-svaicovschi <bogdan.svaicovschi@softwire.com>
2026-07-07 17:42:03 +0300

    Refactor computeFibonacciNumber and related functions

--- src/fibonacci.ts
+++ src/fibonacci.ts
@@ -1,7 +1,5 @@
 export const computeFibonacciNumber = (position: number | null, recursion: boolean = false): number => {
-   if (recursion) {
-        return recursiveFibonacci(1, 1, position - 2);
-    }
+   
     let notNullPosition = position;
     if (notNullPosition === null) {
         notNullPosition = 1;   
@@ -16,6 +14,10 @@ export const computeFibonacciNumber = (position: number | null, recursion: boole
 
     if (notNullPosition <= 2) {
         return 1;
+    }
+
+   if (recursion) {
+        return recursiveFibonacci(1, 1, position - 2);
     }
 
     let i = 1;
